9860b897be5137c9868b7f997776abfe10580c39
ANUBHAB CHAUDHURI <91276548+anubhabch10@users.noreply.github.com>
2025-01-17 06:20:12 +0000

    strings

---
+++ 01_basics/strings.js
@@ -0,0 +1,24 @@
+const name = "anubhab"
+const repoCount = 20
+
+//console.log(name + repoCount + "Value");
+
+//console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+
+// declaration of string
+
+const gamename = new String('abcd')
+
+console.log(gamename[0]);
+
+console.log(gamename.__proto__);
+
+
+console.log(gamename.length);
+
+console.log(gamename.toUpperCase());
+
+console.log(gamename.charAt(2));
+
+console.log(gamename.indexOf('b'));
