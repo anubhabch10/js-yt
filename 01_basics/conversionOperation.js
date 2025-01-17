be0f35348167783fdd78333436b3b99e020586e2
ANUBHAB CHAUDHURI <91276548+anubhabch10@users.noreply.github.com>
2025-01-17 04:14:10 +0000

    operations of datatypes

--- 01_basics/conversionOperation.js
+++ 01_basics/conversionOperation.js
@@ -16,4 +16,42 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 
 let somenumber = 33
 let stringnumber = String(somenumber)
-console.log(stringnumber);
+//console.log(stringnumber);
+
+// ********************************************** OPERATIONS *********************************************************
+
+
+let value = 3
+let negvalue = -value
+// console.log(negvalue);
+
+let str1 = "Hello"
+let str2 = " Anubhab"
+
+let str3 = str1 + str2
+
+//console.log(str3);
+
+/*console.log(1 + "2");
+console.log("1" + 2);
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+*/
+let gc = 100
+gc1 = ++gc;
+gc2 = gc++;
+//console.log(gc1);
+//console.log(gc2);
+
+let x = 3;
+const y = x++;
+
+//console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+//console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
+
