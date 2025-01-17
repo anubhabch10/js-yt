9feb6d7fb5fa8d6729b9fd3f6d9e78eb6d1c7958
ANUBHAB CHAUDHURI <91276548+anubhabch10@users.noreply.github.com>
2025-01-17 05:24:09 +0000

    datatypes

--- 01_basics/datatypes.js
+++ 01_basics/datatypes.js
@@ -2,7 +2,35 @@
 
 // alert(3 + 3)
 
-let name = "Hitesh" //STRING
-let age = 23 //number
-let isLoggedIn = false // boolean
+//let name = "Hitesh" //STRING
+//let age = 23 //number
+//let isLoggedIn = false // boolean
 
+// ***************************************//
+
+// PRIMITIVE
+
+let temp;
+
+const id = Symbol('123');
+const id2 = Symbol('123')
+
+//console.log(id === id2);
+
+const bignumber = 34234235234545435453464n
+
+//REFERENCE OR NON-PRIMITIVE
+//ARRAY
+const heroes = ["abc", "def", "ghi"];
+// OBJECT
+let myobj = {
+    name : "anubhab",
+    age : 22,
+}
+// FUNCTION
+const myfunction = function(){
+//    console.log("Hello world");
+    
+}
+
+console.log(typeof temp);
