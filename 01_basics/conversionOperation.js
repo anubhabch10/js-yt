35a855f22905c09dafe139eff3b2f28ea7c2d063
ANUBHAB CHAUDHURI <91276548+anubhabch10@users.noreply.github.com>
2025-01-17 03:32:54 +0000

    conversion of datatypes

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,19 @@
+let score = "33abc"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+// console.log(booleanIsLoggedIn);
+
+let somenumber = 33
+let stringnumber = String(somenumber)
+console.log(stringnumber);
