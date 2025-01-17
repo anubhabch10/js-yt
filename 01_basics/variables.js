cd4710944ea34de8d35a79b3bfa7792d3b46f776
ANUBHAB CHAUDHURI <91276548+anubhabch10@users.noreply.github.com>
2025-01-17 02:45:20 +0000

    container config done

---
+++ 01_basics/variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail = "anubhab@google.com"
+var accountPassword = "12345"
+accountCity = "Kolkata"
+let accountState;
+
+// accountId = 2 
+
+accountEmail = "hc@hc.com"
+accountPassword = "2121212"
+accountCity = "Bengaluru"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
